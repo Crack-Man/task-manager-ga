3deca6c6c52daefa7bee4d839b0028fe6863eda6
Crack-Man <toxa-18.10@yandex.ru>
2023-04-04 11:29:48 +1000

    release

---
+++ ga.py
@@ -0,0 +1,87 @@
+import numpy as np
+import random as rnd
+
+class GA:
+    def random_spec(self):
+        return np.random.randint(1, self.m, self.n)
+
+    def __init__(self, popsize, n, m, o_n, t_n, k_m, k_select=1, selection_type="tour"):
+        self.popsize = popsize
+        self.n = n
+        self.m = m
+        self.o_n = o_n
+        self.t_n = t_n
+        self.k_m = k_m
+        self.k_select = k_select
+        self.best_p = None
+        self.best_rate = None
+        self.selection_type = selection_type
+        self.p = np.array([self.random_spec() for _ in range(self.popsize)])
+
+    def fit(self, s):
+        times = [0 for _ in range(self.m)]
+        for ss, oo, tt in zip(s, self.o_n, self.t_n):
+            times[ss - 1] += self.k_m[ss - 1, oo - 1] * tt
+        return max(times)
+
+    def two_point_crossover(self, i, j):
+        arange = np.arange(self.n)
+        np.random.shuffle(arange)
+        first, second = arange[0:2]
+        first, second = min(first, second), max(first, second)
+        if np.random.random() < 0.5:
+            return np.concatenate([self.p[i, :first + 1], self.p[j, first + 1:second], self.p[i, second:]])
+        return np.concatenate([self.p[j, :first + 1], self.p[i, first + 1:second], self.p[j, second:]])
+
+    def crossover(self, i, j):
+        t = np.random.randint(0, self.m)
+        return np.concatenate([self.p[i, :t], self.p[j, t:]])
+
+    def selection_roulette(self, fit_rates, p1):
+        weights = fit_rates / np.sum(fit_rates)
+        idxs = np.arange(0, self.popsize)
+        choice = np.random.choice(idxs, self.k_select, p=weights)
+        return p1[choice]
+
+    def selection_range(self, p1):
+        a = 1.5
+        b = 2 - a
+        p = [1 / self.popsize * (a - (a - b) * (i - 1) / (self.popsize - 1)) for i in range(self.popsize)]
+        idxs = np.arange(0, self.popsize)
+        choice = rnd.choices(population=idxs, weights=p, k=self.k_select)
+        return p1[choice]
+
+    def sort(self, a, b):
+        idx = np.argsort(a)
+        return a[idx], b[idx]
+
+    def step(self):
+        # Find best
+        fit_rates = []
+        for s in self.p:
+            fit_rates.append(self.fit(s))
+
+        fit_rates_sort, p1 = self.sort(np.array(fit_rates), self.p)
+        self.best_p = p1[0]
+        self.best_rate = fit_rates_sort[0]
+
+        # Selection
+        if self.selection_type == "tour":
+            p1 = list(p1[:self.k_select])
+        elif self.selection_type == "roulette":
+            p1 = list(self.selection_roulette(fit_rates_sort, p1))
+        elif self.selection_type == "range":
+            p1 = list(self.selection_range(p1))
+
+        # Crossover
+        for _ in range(self.popsize - self.k_select):
+            i, j = np.random.randint(0, self.popsize - 1, 2)
+            p1.append(self.two_point_crossover(i, j))
+        self.p = np.array(p1)
+
+        # Mutation
+        for i in range(self.k_select, self.popsize):
+            if np.random.random() < 0.1:
+                self.p[i] = self.random_spec()
+
+        return self.best_rate, self.best_p
---
+++ main.py
@@ -0,0 +1,24 @@
+from ga import GA
+import numpy as np
+
+with open("input.txt", "r") as f:
+    n = int(f.readline())
+    o_n = np.array(list(map(int, f.readline().split())))
+    t_n = np.array(list(map(float, f.readline().split())))
+    m = int(f.readline())
+
+    k_m = []
+    for _ in range(m):
+        k_m.append(list(map(float, f.readline().split())))
+    k_m = np.array(k_m)
+
+popsize = 20
+ga = GA(popsize, n, m, o_n, t_n, k_m, k_select=3, selection_type="tour")
+# ga = GA(popsize, n, m, o_n, t_n, k_m, k_select=3, selection_type="roulette")
+# ga = GA(popsize, n, m, o_n, t_n, k_m, k_select=3, selection_type="range")
+step = None
+for i in range(int(1e5)):
+    step = ga.step()
+# if not i % 100:
+    #     print(*step)
+print(*step)
